e7a9ad081bc4fb24210775b0522a7a6327cf342b
sonal kamaliya <sonikamaliya@gmail.com>
2024-07-06 12:34:33 +0530

    Add rspec for add method of string calculator

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,13 +1,19 @@
+# Calculate numbers from string if no negative number present
 class StringCalculator
-  def initialize(numbers)
-      @numbers = numbers
+  def self.add(string_arr)
+    @string_arr = string_arr
+
+    extract_numbers
+
+    if @negative_numbers.empty?
+      return @numbers.map(&:to_i).reduce(:+)
+    else
+      raise "Negative numbers not allowed: #{@negative_numbers.join(', ')}"
+    end
   end
 
-  def self.add(@number)
-
+  def self.extract_numbers
+    @numbers = @string_arr.split(/[^-\d]/).reject(&:empty?)
+    @negative_numbers = @string_arr.scan(/-\d/)
   end
-
-  private
-
-  def extract_numbers(number_string)
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -2,10 +2,46 @@ require './string_calculator.rb'
 
 RSpec.describe StringCalculator do
   describe 'String Calculator' do
-    it "Initilize number string" do 
-      string_calc = StringCalculator.new(number="1,2")
-      # expect(string_calc.number).to eq
-      allow(StringCalculator).to receive(:new).with(number="1,2").and_return(string_calc)
+    it "Initilize number string with add method" do 
+      allow(StringCalculator).to receive(:add)
+    end
+  end
+
+  describe '#add method' do
+    it "accept string" do
+      allow(StringCalculator).to receive(:add).with("1,2")
+    end
+
+    it "return nil for blank string" do
+      expect(StringCalculator.add("")).to eq(nil)
+    end
+
+    it "return self for single number" do
+      expect(StringCalculator.add("0")).to eq(0)
+      expect(StringCalculator.add("10")).to eq(10)
+    end
+
+    it "sum two number if no negative include in string" do
+      expect(StringCalculator.add("1,2")).to eq(3)
+    end
+
+    it "reject string delimiters and sum value from string" do
+      expect(StringCalculator.add("1,2")).to eq(3)
+      expect(StringCalculator.add("1,2,3")).to eq(6)
+    end
+
+    it "reject any tyoe of delimiters and sum value from string" do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+      expect(StringCalculator.add("//[*][%]\n1*2%3")).to eq(6)
+    end
+
+    it "return/raise exception if string contains negative number" do
+      expect { StringCalculator.add("1,-2") }.to raise_error("Negative numbers not allowed: -2")
+    end
+
+    it "return/raise exception with comma seperated numbers if string contains more then 1 negative numbers" do
+      expect { StringCalculator.add("1,-2, -3") }.to raise_error("Negative numbers not allowed: -2, -3")
     end
   end
 end
