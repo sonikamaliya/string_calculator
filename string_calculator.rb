2d7f53bc9149bbca623c3d148d0a870e1ca8d94a
sonikamaliya <sonikamaliya@gmail.com>
2024-11-03 18:12:58 +0530

    Add comments for better understanding of code

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,15 +1,19 @@
 class StringCalculator
+
+  # Adding numbers from given string seperated by delimeters like [",". "\n", ";"]
   def add(str)
     return 0 if str.empty?
     
     delimiters = [",", "\n"]
+
+    # if string contains custom delimeters get the custom delimeter and numbers from the string
     if str.start_with?("//")
       divide_string = str.split("\n", 2)
       delimiters << divide_string[0][2..-1]
       str = divide_string[1]
     end
 
-    scan_pattern = Regexp.union(delimiters)
+    scan_pattern = Regexp.union(delimiters) #create pattern for match with the given string 
     numbers_list = str.split(scan_pattern).map(&:to_i)
 
     negatives = numbers_list.select{|x| x.negative? }
